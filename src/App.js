9ee6969b94388601c8c7995613826a3f3dba22c2
Rahul barman <75018120+rahul0179@users.noreply.github.com>
2022-09-09 04:21:03 +0600

    useState,useEffect

--- src/App.js
+++ src/App.js
@@ -1,16 +1,94 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
 
 function App() {
 
   return (
     <div className="App">
-
+      <ExtrernalUsers></ExtrernalUsers>
 
 
     </div>
   );
 }
+
+function ExtrernalUsers() {
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(jData => setUser(jData))
+  }, [])
+  return (
+    <div>
+      <h1>Extrenal Users</h1>
+
+
+      {
+        user.map(u => <User name={u.name} email={u.email}></User>)
+      }
+    </div>
+
+  )
+}
+
+function User(props) {
+  return (
+    <div style={{ border: '2px solid red', margin: '4px' }}>
+      <h3>Name:{props.name}</h3>
+      <h3>Email: {props.email}</h3>
+    </div>
+
+
+
+  )
+}
+
+
+
+
+/* ----------------------------------------------------
+             useState()
+---------------------------------------------------             
+function App() {
+
+  return (
+    <div className="App">
+      <Counter></Counter>
+
+
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(55)
+
+
+  const increaseCount = () => setCount(count + 1)
+  const decreaseCount = () => setCount(count - 1)
+
+
+
+   const increaseCount = () => {
+     const newCount = count + 1;
+     setCount(newCount); 
+} 
+
+  return (
+    <div>
+      <h1>Count : {count}</h1>
+      <button onClick={increaseCount}>Incress</button>
+      <button onClick={decreaseCount}>Decress</button>
+    </div>
+  )
+}
+----------------------------------------------------
+ */
+
+
 /* -------------------------------------------------------------------------------
                 make component / props /dynamic data set
  ------------------------------------------------------------------------------
