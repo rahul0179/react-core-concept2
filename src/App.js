2a9b6ff05a8ae217a160d8db8fd6ad3c2fd7c19e
Rahul barman <75018120+rahul0179@users.noreply.github.com>
2022-09-09 01:04:14 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -2,24 +2,56 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
     </div>
   );
 }
+/* -------------------------------------------------------------------------------
+                make component / props /dynamic data set
+ ------------------------------------------------------------------------------
+const students = ['Rahul barman', 'Zubayer Tolon', 'Turja', 'Arnob']
+const people = [
+  { name: 'Rahul barman', varsity: 'Diu' },
+  { name: 'Sharmin sadiya', varsity: 'Diu' },
+  { name: 'MD Rakib', varsity: 'Diu' },
+  { name: 'Rakibul Hasan', varsity: 'Diu' },
+  { name: 'Mhathir Shipon', varsity: 'Diu' },
+]
+
+function App() {
+  return (
+<div className="App">
+
+{
+  people.map(p => <Product name={p.name} varsity={p.varsity}></Product>)
+}
+
+{
+  students.map(student => <Product name={student}></Product>)
+}
+</div>
+  );
+}
+
+
+
+function Product(props) {
+  return (
+    <div className='product'>
+      <p>{props.name}</p>
+      <p>{props.varsity}</p>
+    </div>
+  )
+}
+--------------------------------------------------------------------------------- */
+
+
+
+
 
 export default App;
